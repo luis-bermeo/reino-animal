0acf3faaf0ccde81dc3ddc98037da2953f2a220a
Luis_ <Luis_@ELE>
2025-03-05 13:11:33 +0100

    Introducida documentación  CORREGIDA a las clases y métodos del proyecto.

--- src/main/java/es/iesjoseplanes/ed1dawdist/tarea4/Animal.java
+++ src/main/java/es/iesjoseplanes/ed1dawdist/tarea4/Animal.java
@@ -1,25 +1,25 @@
 package es.iesjoseplanes.ed1dawdist.tarea4;
 
-/*Clase que crea un animal genérico. Tiene un nombre y métodos para obtenerlo y modificarlo*/
+/**Clase que crea un animal genérico. Tiene un nombre y métodos para obtenerlo y modificarlo*/
 public class Animal {
 
     private String nombre;
     
-    /*Obtiene el nombre del animal
+    /**Obtiene el nombre del animal
     * @return Nombre del animal
     */
     public String getNombre() {
         return nombre;
     }
     
-    /*Establece el nombre del animal
+    /**Establece el nombre del animal
     * @parametro nombre Nombre del animal
     */
     public void setNombre (String nombre) {
         this.nombre = nombre;
     }
 
-    //Constructor de la clase animal
+    /**Constructor de la clase animal*/
     public Animal (String nombre) {
         this.nombre = nombre;
     }
@@ -36,12 +36,12 @@ public class Animal {
         System.out.println("Originar nuevos seres vivos");
     }
 
-    /*Método que muestra la relacion entre loso animales creados*/
+    /**Método que muestra la relacion entre loso animales creados*/
     void relacionar(Animal a) {
         System.out.println("Con el animal: " + a.nombre);
     }
     
-    /*Imprime por pantalla la cadena de caracteres del objeto Animal
+    /**Imprime por pantalla la cadena de caracteres del objeto Animal
     @return Cadena con el nombre del animal*/
     String ToString() {
         return this.nombre;
